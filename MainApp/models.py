a653381ec03015d028aa4e736575cd49234296e1
xm4dn355 <xm4dn355x@gmail.com>
2021-08-06 20:20:52 +0300

    add default value for creation date field of Snippet model

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 
@@ -5,4 +6,4 @@ class Snippet(models.Model):
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30)
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(default=datetime.now())
