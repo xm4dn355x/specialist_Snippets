51e153009314b990e665836e0eb4082d0043e67a
xm4dn355 <xm4dn355x@gmail.com>
2021-08-08 10:53:19 +0300

    add snippet add form

---
+++ MainApp/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Snippet
+
+
+class SnippetForm(ModelForm):
+   class Meta:
+       model = Snippet
+       # Описываем поля, которые будем заполнять в форме
+       fields = ['name', 'lang', 'code']
