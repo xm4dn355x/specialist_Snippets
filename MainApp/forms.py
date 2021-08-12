501dd06bfe5db385267870e13c2cca6acb5540b2
xm4dn355 <xm4dn355x@gmail.com>
2021-08-12 19:42:55 +0300

    commits for heroku

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,5 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, ChoiceField
 from .models import Snippet
+
+
+LANG_CHOICES = (
+    ('py', 'Python'),
+    ('js', 'JavaScript'),
+    ('cpp', 'C++')
+)
 
 
 class SnippetForm(ModelForm):
@@ -7,3 +14,6 @@ class SnippetForm(ModelForm):
        model = Snippet
        # Описываем поля, которые будем заполнять в форме
        fields = ['name', 'lang', 'code']
+       widgets = {
+           'name': TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Название сниппета'}),
+       }
